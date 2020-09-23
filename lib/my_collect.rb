598b0cc71bac5d3cb3e5805bbc15fb6ee09465dd
Ryan K Burns <ryankburns@gmail.com>
2020-09-23 16:59:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-    collection << yield(array[i].first.upcase)
+    collection << yield(array[i].upcase)
+    collection << yield(array[i].split(" ").first)
       i += 1
     end
     collection

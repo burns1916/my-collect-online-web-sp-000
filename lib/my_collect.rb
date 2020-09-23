049cae8c838bf15a593ca117fd47b51ee6ad0d77
Ryan K Burns <ryankburns@gmail.com>
2020-09-23 16:52:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+  i = 0
+  while i < array.length
+    collection = []
+    yield(array[i])
+    do collection << array.upcase
+      i += 1
+    end
+    collection
+end
 
